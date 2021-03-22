3b7e0227ff02379290061774d474553b0151134a
Andrei Racasan <aracasan@outlook.com>
2021-03-22 14:52:20 +0000

    add search icon

--- src/comps/ColleagueForm.js
+++ src/comps/ColleagueForm.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Button, TextField } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import {v4 as uuid} from "uuid"; 
 
 function ColleagueForm({addColleague}) {
@@ -24,6 +25,7 @@ function ColleagueForm({addColleague}) {
 
   return (
     <form className="form" onSubmit={handleSubmit}>
+      <SearchIcon style={{marginRight: 6}} />
       <TextField label="colleague" name="colleague" type="text" value={colleague.name} onChange={handleTaskInputChange} />
       <Button type="submit">Add</Button>
     </form>
